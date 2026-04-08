9cccaebd13985023327aa97076d2ef6b728aa1da
cr2hchoi <20231046@sungshin.ac.kr>
2026-04-08 16:49:42 +0900

    chore : main.css 경로 수정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
-import './assets/styles/main.css';
+import '../src/assets/styles/main.css';
 
 const app = createApp(App);
 
